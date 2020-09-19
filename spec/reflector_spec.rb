a6d782eb53a42bbe585b60870426c991c643afc6
William Akin <wakin66@gmail.com>
2020-09-19 02:10:52 -0400

    Wrote specs for Reflector#initialize

--- spec/reflector_spec.rb
+++ spec/reflector_spec.rb
@@ -5,15 +5,24 @@ describe Reflector do
 
     describe "#initialize" do
         context "takes a string as an argument" do
-            it "doesn't raise an argument when given the string 'a' or 'b'."
+            it "doesn't raise an argument when given the string 'a' or 'b'." do
+                expect {Reflector.new("a")}.to_not raise_error
+                expect {Reflector.new("b")}.to_not raise_error
+            end
 
-            it "raises an error when given the wrong arguments"
+            it "raises an error when given the wrong arguments" do
+                expect {Reflector.new("c")}.to raise_error(ArgumentError,"Invalid reflector ID.")
+            end
 
-            it "raises an error when length != 1"
+            it "raises an error when length != 1" do
+                expect {Reflector.new("ab")}.to raise_error(ArgumentError,"Invalid reflector ID.")
+            end
         end
 
         context "is given an argument that is NOT a string" do
-            it "raises an error"
+            it "raises an error" do
+                expect {Reflector.new(1)}.to raise_error(TypeError,"Reflector ID must be a letter.")
+            end
         end
     end
 
