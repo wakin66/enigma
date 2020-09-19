fbaba23ab8d8c71e6fa98ebd856a78f404d6e02e
William Akin <wakin66@gmail.com>
2020-09-19 14:50:27 -0400

    Completed Rotor class

--- lib/rotor.rb
+++ lib/rotor.rb
@@ -2,7 +2,7 @@ class Rotor
     #attr_reader :setting
 
     #These connections were gathered from https://bit.ly/33EWSk6
-    ROTOR_CONNECTIONS = [
+    WIRING_CONNECTIONS = [
         :EKMFLGDQVZNTOWYHXUSPAIBRCJ,
         :AJDKSIRUXBLHWTMCQGZNPYFVOE,
         :BDFHJLCPRTXVZNYEIWGAKMUSQO,
@@ -12,9 +12,11 @@ class Rotor
 
     ALPHA = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-    def initialize(id,pos="A")
+    def initialize(id,pos=1)
         raise(ArgumentError,"Invalid rotor number.") unless [1,2,3,4,5].include?(id)
         @id = id
+        @position = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        @setting = WIRING_CONNECTIONS[@id-1]
         set_position(pos)
     end
 
@@ -22,21 +24,24 @@ class Rotor
         raise(TypeError,"Character must be a letter.") unless char.is_a? String
         raise(ArgumentError,"Invalid character.") unless char.length == 1
         raise(ArgumentError,"Invalid character.") unless ALPHA.include?(char.upcase)
-        new_char = @setting.slice(ALPHA.index(char.upcase))
+        input_pos = ALPHA.index(char.upcase)
+        input_setting = @position[input_pos]
+        output_setting = @setting.slice(ALPHA.index(input_setting))
+        output_pos = @position.index(output_setting)
+        new_char = ALPHA[output_pos]
         rotate
         return new_char
     end
 
-    def set_position(char)
-        @setting = ROTOR_CONNECTIONS[@id-1]
-        char = char.upcase
-        (ALPHA.index(char)).times {rotate}
+    def set_position(pos)
+        raise(ArgumentError,"Invalid rotor position") unless (1..26).include?(pos)
+        (pos-1).times {rotate}
     end
 
     private
 
     def rotate
-        @setting = (@setting[1..-1]+@setting[0]).to_sym
+        @position = @position[1..-1]+@position[0]
     end
 
 end
