fe0ca9e3a9e10ce17cb871e66750a850553705b4
William Akin <wakin66@gmail.com>
2020-09-19 02:21:17 -0400

    Wrote specs for Reflector#get_value

--- spec/reflector_spec.rb
+++ spec/reflector_spec.rb
@@ -28,19 +28,31 @@ describe Reflector do
 
     describe "#get_value" do
         context "is given a string as an argument" do
-            it "doesn't raise an argument when length = 1"
+            it "doesn't raise an argument when length = 1" do
+                expect {reflector.get_value("a")}.to_not raise_error
+            end
 
-            it "raises an error when length != 1"
+            it "raises an error when length != 1" do
+                expect {reflector.get_value("ab")}.to raise_error(ArgumentError,"Invalid reflector ID.")
+            end
 
-            it "raises an error when the string is not a letter"
+            it "raises an error when the string is not a letter" do
+                expect {reflector.get_value("1")}.to raise_error(ArgumentError,"Invalid reflector ID.")
+            end
 
-            it "returns 'Y' when given the string 'A'"
+            it "returns 'Y' when given the string 'A'" do
+                expect(reflector.get_value("A")).to eq("Y")
+            end
 
-            it "returns 'A' when given the string 'Y'"
+            it "returns 'A' when given the string 'Y'" do
+                expect(reflector.get_value("Y")).to eq("A")
+            end
         end
 
         context "is given an argument that is NOT a string" do
-            it "raises an error"
+            it "raises an error" do
+                expect {reflector.get_value(1)}.to raise_error(TypeError,"Reflector ID must be a letter.")
+            end
         end
     end
 
