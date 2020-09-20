48f9f67296ad7941ed0e8270bfae38239b981d34
William Akin <wakin66@gmail.com>
2020-09-20 02:18:10 -0400

    Created constant ALPHA, fixed Plugboard#initialize, wrote Plugboard#get_wires, wrote Plugboard#fill_plugs, created private attr_accessor for @plugs

--- lib/plugboard.rb
+++ lib/plugboard.rb
@@ -1,8 +1,17 @@
 class Plugboard
 
+    ALPHA = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
     def initialize(wires = nil)
+        raise(ArgumentError,"Argument must be a hash") unless ((wires.is_a? Hash) || !wires)
         @plugs = Hash.new
-        fill_wires(wires)
+        if wires
+            wires.each do |pos1,pos2|
+                @plugs[pos1] = pos2
+                @plugs[pos2] = pos1
+            end
+        end
+        fill_plugs
     end
 
     def add_wire(pos1,pos2)
@@ -14,7 +23,18 @@ class Plugboard
     end
 
     def get_wires
+        ignore = Array.new
+        wires = Hash.new
 
+        plugs.each do |k,v|
+            if k!=v
+                if !ignore.include?(k)
+                    ignore << v
+                    wires[k] = v
+                end
+            end
+        end
+        return wires
     end
 
     def get_value(char)
@@ -23,8 +43,10 @@ class Plugboard
 
     private
 
-    def fill_wires(wires)
+    attr_accessor :plugs
 
+    def fill_plugs
+        ALPHA.each_char {|char| plugs[char]=char if !plugs.keys.include?(char)}
     end
 
     def num_wires_used
