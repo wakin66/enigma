a96687355f5b3b1147d00fc9f4d98f2cd2e12d5c
William Akin <wakin66@gmail.com>
2020-09-20 01:41:12 -0400

    Created Plugboard class skeleton

--- lib/plugboard.rb
+++ lib/plugboard.rb
@@ -1,3 +1,33 @@
 class Plugboard
 
+    def initialize(wires = nil)
+        
+    end
+
+    def add_wire(pos1,pos2)
+
+    end
+
+    def remove_wire(wire_num)
+
+    end
+
+    def get_wires
+
+    end
+
+    def get_value(char)
+
+    end
+
+    private
+
+    def fill_wires(wires)
+
+    end
+
+    def num_wires_used
+        
+    end
+
 end
