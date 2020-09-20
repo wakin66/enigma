53197ab61226d6f66683f47214478fd79aa8fb94
William Akin <wakin66@gmail.com>
2020-09-20 06:53:37 -0400

    Created basic spec skeleton

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -2,4 +2,23 @@ require 'enigma'
 
 describe Enigma do
 
+    describe "#initialize" do
+
+    end
+
+    describe "#load_reflector" do
+        it "creates an instance of a reflector"
+    end
+
+    describe "#load_plugboard" do
+        it "creates an instance of a plugboard"
+    end
+
+    describe "#load_rotors" do
+        it "creates an instance of rotor I"
+
+        it "creates an instance of rotor III"
+
+        it "creates an instance of rotor V"
+    end
 end
