24a1325c1306a05f65e6f851cceee57013ca712c
William Akin <wakin66@gmail.com>
2020-09-25 04:52:35 -0400

    Updated Board class for better handling of Rotor positions

--- lib/board.rb
+++ lib/board.rb
@@ -1,5 +1,7 @@
 class Board
 
+    ALPHA = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
     attr_reader :values
 
     def initialize
@@ -11,7 +13,7 @@ class Board
         row_idx = 6
         col_idx = [3,8,13]
 
-        rotor_positions.each.with_index {|pos,idx| values[row_idx][col_idx[idx]] = pos}
+        rotor_positions.each.with_index {|pos,idx| values[row_idx][col_idx[idx]] = ALPHA[pos-1]}
     end
 
     private
@@ -19,15 +21,12 @@ class Board
     attr_writer :values
 
     def fill_board
-        rotor_positions = [1,1,1]
         row_12 = ["Q","W","E","R","T","Z","U","I","O"]
         row_13 = ["A","S","D","F","G","H","J","K"]
         row_14 = ["P","Y","X","C","V","B","N","M","L"]
         values.each.with_index do |row,row_idx|
             row.each_index do |col_idx|
-                if row_idx == 6 && [3,8,13].include?(col_idx)
-                    values[row_idx][col_idx] = rotor_positions[(col_idx-2)/4]
-                elsif row_idx == 12 && col_idx.even?
+                if row_idx == 12 && col_idx.even?
                     values[row_idx][col_idx] = row_12[col_idx/2]
                 elsif row_idx == 13 && col_idx.odd?
                     values[row_idx][col_idx] = row_13[col_idx/2]
