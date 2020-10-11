71d9ac7553367cfe7ec9e794af18fd21b2bf6ad2
William Akin <wakin66@gmail.com>
2020-10-11 18:32:20 -0400

    Updated string_alpha.rb to use regex

--- lib/string_alpha.rb
+++ lib/string_alpha.rb
@@ -1,10 +1,8 @@
-ALPHA = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-
 class String
 
     def alpha
         temp_arr = self.chars
-        temp_arr.keep_if {|char| ALPHA.include?(char.upcase)}
+        temp_arr.keep_if {|char| cahr.match?(/[A-Za-z]*/)}
         temp_arr.join
     end
 
